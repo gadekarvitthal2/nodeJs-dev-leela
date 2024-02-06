bb676112d7e36c05732304a4bc842cf38cba0ca3
Vitthal Gadekar <gadekarvitthal2@gmail.com>
2024-02-06 15:52:26 +0530

    - Render Products Data in the EJS Template using For loop

--- routes/home.js
+++ routes/home.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const home = express.Router();
 const rooDir = require("../utility/util");
-app.use(express.static(path.join(__dirname, "public")));
+const products = require('../utility/products');
 
+app.use(express.static(path.join(__dirname, "public")));
 home.get("/", (req, res, next) => {
-  res.sendFile(path.join(rooDir, "views", "home.html"));
+  // res.sendFile(path.join(rooDir, "views", "home.html"));
+   res.render('home',{productTitle:products.productTitle,productsData:products.productList});
 });
 
 module.exports = home;
---
+++ utility/products.js
@@ -0,0 +1,18 @@
+const productList = [{
+    name:'Gogles',
+    price:'250'
+},{
+    name:'Mobile',
+    price:'25000'
+},{
+    name:'Wallet',
+    price:'500'
+}];
+const productFunc = (product)=> {
+    products.push(product);
+}
+
+module.exports = {
+    productTitle:'E Commerce Project',
+    productList:productList
+}
